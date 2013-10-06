7329bb24248dc406c3c3764671a3936aac24b2a9
RadX <ralobba@gmail.com>
2013-10-06 15:36:00 +0800

    Create managed object subclass

---
+++ Category.m
@@ -0,0 +1,18 @@
+//
+//  Category.m
+//  MoneyMemory
+//
+//  Created by Rex Jason Alobba on 6/10/13.
+//  Copyright (c) 2013 Rex Jason Alobba. All rights reserved.
+//
+
+#import "Category.h"
+
+
+@implementation Category
+
+@dynamic id;
+@dynamic limit;
+@dynamic name;
+
+@end
