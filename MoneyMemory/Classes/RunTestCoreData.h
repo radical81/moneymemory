5b564647ad5713c68ef1afc7689b3297cc9abada
RadX <ralobba@gmail.com>
2013-10-07 22:13:09 +0800

    RunTestCoreData class

---
+++ MoneyMemory/Classes/RunTestCoreData.h
@@ -0,0 +1,13 @@
+//
+//  RunCoreData.h
+//  MoneyMemory
+//
+//  Created by Rex Jason Alobba on 6/10/13.
+//  Copyright (c) 2013 Rex Jason Alobba. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RunCoreData : NSObject
+
+@end
---
+++ MoneyMemory/Classes/RunTestCoreData.m
@@ -0,0 +1,13 @@
+//
+//  RunCoreData.m
+//  MoneyMemory
+//
+//  Created by Rex Jason Alobba on 6/10/13.
+//  Copyright (c) 2013 Rex Jason Alobba. All rights reserved.
+//
+
+#import "RunCoreData.h"
+
+@implementation RunCoreData
+
+@end
